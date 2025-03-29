cfa0d0ed80ae26edeb53bd97634330ea511c5348
Utkarshbajpai07 <115313095+Utkarshbajpai07@users.noreply.github.com>
2025-03-29 12:51:39 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
-docker build -t $JOB_NAME:$BUILD_ID .
+docker build -t django:$BUILD_ID .
 
-docker tag $JOB_NAME:$BUILD_ID utkarshbajpai07/$JOB_NAME:$BUILD_ID
+docker tag django:$BUILD_ID utkarshbajpai07/django:$BUILD_ID
 
-docker tag $JOB_NAME:$BUILD_ID utkarshbajpai07/$JOB_NAME:latest
+docker tag django:$BUILD_ID utkarshbajpai07/django:latest
 
-docker push utkarshbajpai07/$JOB_NAME:$BUILD_ID
+docker push utkarshbajpai07/django:$BUILD_ID
 
-docker push utkarshbajpai07/$JOB_NAME:latest
+docker push utkarshbajpai07/django:latest
 
-docker rmi -f $JOB_NAME:$BUILD_ID utkarshbajpai07/$JOB_NAME:$BUILD_ID utkarshbajpai07/$JOB_NAME:latest
+docker rmi -f django:$BUILD_ID utkarshbajpai07/django:$BUILD_ID utkarshbajpai07/django:latest

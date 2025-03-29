bfa4bb3423b002ddc496a8f99909ff077211eab7
Utkarshbajpai07 <115313095+Utkarshbajpai07@users.noreply.github.com>
2025-03-29 12:28:45 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 docker build -t $JOB_NAME:$BUILD_ID .
 
-docker tag $JOB_NAME:$BUILD_ID jacksneel/$JOB_NAME:$BUILD_ID
+docker tag $JOB_NAME:$BUILD_ID utkarshbajpai07/$JOB_NAME:$BUILD_ID
 
-docker tag $JOB_NAME:$BUILD_ID jacksneel/$JOB_NAME:latest
+docker tag $JOB_NAME:$BUILD_ID utkarshbajpai07/$JOB_NAME:latest
 
-docker push jacksneel/$JOB_NAME:$BUILD_ID
+docker push utkarshbajpai07/$JOB_NAME:$BUILD_ID
 
-docker push jacksneel/$JOB_NAME:latest
+docker push utkarshbajpai07/$JOB_NAME:latest
 
-docker rmi -f $JOB_NAME:$BUILD_ID jacksneel/$JOB_NAME:$BUILD_ID jacksneel/$JOB_NAME:latest
+docker rmi -f $JOB_NAME:$BUILD_ID utkarshbajpai07/$JOB_NAME:$BUILD_ID utkarshbajpai07/$JOB_NAME:latest
